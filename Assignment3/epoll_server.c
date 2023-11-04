e391ebf67bccd5b27e5afeec34e19e9bef00c3fc
Akanksha Singal <akanksha21008@iiitd.ac.in>
2023-11-04 16:27:32 +0530

    added select, poll and epoll

--- Assignment3/epoll_server.c
+++ Assignment3/epoll_server.c
@@ -1,24 +1,24 @@
 // include statements used from the link provided
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/epoll.h>
 #include <errno.h>
 #include <syslog.h>
 #include <unistd.h>
 #include <stdbool.h>
-#include <sys/select.h>
 #include <ctype.h>
 #include <stdint.h>
 #include <time.h>
 
-
 #define SERVER_PORT 8080
 #define BACKLOG 4000
+#define MAX_EVENTS 4000
 
 long long factorial(long long n){
 
@@ -31,7 +31,6 @@ long long factorial(long long n){
 }
 
 int main (){
-
     int listener, optval = 1;
     socklen_t length;
     struct sockaddr_in server_address, client_address;
@@ -66,29 +65,36 @@ int main (){
         perror("listen erro\n");
     }
 
+    int efd;
+    if ((efd = epoll_create1 (0)) == -1){
+        perror ("epoll_create1 error");
+    }
+	
+    struct epoll_event ev, ep_event [4001];
+
+    ev.events = EPOLLIN;
+    ev.data.fd = listener;
+    if (epoll_ctl (efd, EPOLL_CTL_ADD, listener, &ev) == -1){
+        perror ("epoll_ctl error");
+    }
+
+    int nfds = 0;
 
     socklen_t addrlen;
-    fd_set fds, readfds;
-    FD_ZERO(&fds);
-    FD_SET(listener, &fds);
-    int fdmax = listener;
-    
-    
 
-    while(1){
-
-        readfds = fds;
-
-        if( select(fdmax + 1 , &readfds, NULL, NULL, NULL) == -1){
-            perror("select error");
-        }
+    while (1) {
         
-        for( int fd = 0; fd < (fdmax + 1); fd++){
+        if ((nfds = epoll_wait (efd, ep_event, 4001,  -1)) == -1){
+            perror ("epoll_wait error");
+        }
             
-            if( FD_ISSET( fd, &readfds)){  
 
-                if( fd == listener){
-                    addrlen = sizeof(client_address);
+        for (int i = 0; i < nfds; i++) {
+
+            if 	((ep_event [i].events & EPOLLIN) == EPOLLIN) {
+                
+                if (ep_event [i].data.fd == listener) {
+                    addrlen = sizeof (client_address);
                     int fd_new;
                     fd_new = accept(listener, (struct sockaddr*)&client_address, &addrlen);
                     if(fd_new < 0){
@@ -96,37 +102,27 @@ int main (){
                         exit(1);
                     }
 
+                    ev.events = EPOLLIN;
+                    ev.data.fd = fd_new;
+                    if( epoll_ctl( efd, EPOLL_CTL_ADD, fd_new, &ev) == -1){
+                        perror("epoll_ctl error");
+                    }
+
                     char *ip_addr = inet_ntoa(clienAddr.sin_addr);
                     int port = ntohs(clienAddr.sin_port);
                     printf("Connection IP : %s: and PORT : %d\n", ip_addr, port);
 
-                    FD_SET(fd_new, &fds);
-                    if( fd_new > fdmax){
-                        fdmax = newsocket;
-                    }
-                    
 
                 }
-                else{  
+                else{
+                    
                     memset(&recv_message, '\0', 1024);
+                    ssize_t numbytes = recv (ep_event [i].data.fd, &recv_message, sizeof (struct message), 0);
 
-                    ssize_t numbytes = recv( fd, &recv_message, sizeof(recv_message), 0);
                     if (numbytes == -1){
                         perror("recv error");
                         exit(1);
                     }
-                    else if (numbytes == 0){
-                        fprintf(stderr, "socket closed by client", fd);
-                        if (close(fd) == -1){
-                            perror("close error");
-                            exit(1);
-                        }
-                        FD_CLR(fd, &fds);
-                    }
-                    else{
-                        recv_message[numbytes] = '\0';
-                    }
-
                     long long num = atoi(recv_message);
                     if (num > 20){
                         sprintf(recv_message, "%lld", factorial(20));
@@ -135,19 +131,19 @@ int main (){
                     else{
                         sprintf(recv_message, "%lld", factorial(num));
                     }
-                    
-                    
                     if (send(fd, &recv_message, sizeof(recv_message), 0) < 0){
                         perror("send error");
                         exit(1);
                     }
 
+
                 }
+            
             }
         }
-        
     }
-    
+
     close(listener);
     return 0;
+
 }
